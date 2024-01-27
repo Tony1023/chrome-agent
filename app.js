18eeae650e7068f491e58af26b691747d3c4cafc
Tony Lu <tony1023lv@gmail.com>
2024-01-27 02:34:43 -0800

    Getting the page correctly and ask GPT for the "new" button in the HTML page.

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const puppeteer = require('puppeteer');
 const readline = require('readline');
+const fs = require('fs');
 require('dotenv').config()
-const domParser = require('dom-parser');
 const openai = require('openai');
+const { setTimeout } = require("timers/promises");
+const cheerio = require('cheerio');
 
 const getTemporaryLoginCode = () => {
   const rl = readline.createInterface({
@@ -17,7 +19,7 @@ const getTemporaryLoginCode = () => {
 }
 
 // Using the default reading list template when you first create your personal workspace on notion.
-const getReadingListFromNotion = async () => {
+const openNotionPage = async () => {
   const browser = await puppeteer.launch({ headless: 'new' });
   const page = await browser.newPage();
 
@@ -54,20 +56,21 @@ const getReadingListFromNotion = async () => {
   await page.waitForNavigation({ waituntil: 'domcontentloaded' });
   await page.waitForSelector('div ::-p-text(Reading List)');
   await page.waitForSelector('div ::-p-text(Completed)');
-  const tableHTML = await page.$eval('div.notion-table-view', e => e.innerHTML);
-  await browser.close();
-  return tableHTML;
+  await setTimeout(10000);
+
+  // const tableHTML = await page.$eval('div.notion-table-view', e => e.innerHTML);
+  return page;
 };
 
 const askGPT = async htmlContent => {
   const client = new openai.OpenAI({ apiKey: process.env.OPENAI_API_KEY })
 
   const completion = await client.chat.completions.create({
-    model: "gpt-4",
+    model: "gpt-4-turbo-preview",
     messages: [
-      { role: "system", content: "You are HTML language master that can understand HTML files and see what the page is about." },
-      { role: 'user', content: 'What are the books to read in the below page?' },
+      { role: "system", content: "You are HTML parser that can understand HTML files and find HTML elements to interact for specific actions required. For example, you can locate the delete button if the action is to delete an item." },
       { role: 'user', content: htmlContent },
+      { role: 'user', content: 'In the above page, can you please locate the HTML element to add a new reading item in the page?' },
     ],
   });
 
@@ -75,12 +78,34 @@ const askGPT = async htmlContent => {
 }
 
 const main = async () => {
-  let tableHTML = await getReadingListFromNotion();
-  tableHTML = tableHTML.replace(/style="[^\"]*"/gi, '');
-  tableHTML = tableHTML.replace(/d="[^\"]*"/gi, '');
-  console.log(tableHTML);
+  let content;
+  let page = await openNotionPage();
+  try {
+    content = await page.content();
+    fs.writeFileSync('./page_content.html', content);
+  } catch (e) {
+    console.log(e);
+  }
 
-  console.log((await askGPT(tableHTML)).choices[0].message.content);
+  try {
+    content = fs.readFileSync('./page_content.html', { encoding: 'utf8' });
+  } catch (e) {
+    console.log(e);
+  }
+
+  let $ = cheerio.load(content);
+  $('*').each((i, e) => {
+    const allAttributes = e.attribs;
+    e.attribs = {};
+    if (allAttributes.id) {
+      e.attribs['id'] = allAttributes.id;
+    }
+    if (allAttributes.class) {
+      e.attribs['class'] = allAttributes.class;
+    }
+  });
+  console.log((await askGPT($.html())).choices[0].message.content);
+
 };
 
 main();
